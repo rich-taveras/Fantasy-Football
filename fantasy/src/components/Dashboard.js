a59088d07cfff54a0ee2a9e5f93b7a2d3b77647f
rich-taveras <rtaveras@yahoo.com>
2023-11-21 22:01:13 -0500

    added dashboard page

---
+++ fantasy/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {/* Aquí puedes agregar contenido específico del usuario autenticado */}
+    </div>
+  );
+};
+
+export default Dashboard;
